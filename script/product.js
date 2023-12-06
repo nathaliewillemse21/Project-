6e719c9bea7a7c034845c62c3c0dc664200a28fe
nathaliewillemse21 <njpwillemse69@gmail.com>
2023-12-06 16:48:04 +0200

    Done

--- script/product.js
+++ script/product.js
@@ -12,7 +12,7 @@ let Products = JSON.parse(localStorage.getItem('product'))
         {
           image: 'https://i.postimg.cc/W1mV8yrp/ichigo-daifuku.jpg',
           id: 0,
-          make: 'Ichigo Daifuku',
+          name: 'Ichigo Daifuku',
           description:
             'It is filled with a sweet red bean paste and a strawberry.',
           price: 60,
@@ -20,7 +20,7 @@ let Products = JSON.parse(localStorage.getItem('product'))
         {
           image: 'https://i.postimg.cc/05HyKhGL/sakura-mochi.jpg',
           id: 1,
-          make: 'Sakura Mochi ',
+          name: 'Sakura Mochi ',
           description:
             'This classic mochi is wrapped in a pickled sakura leaf.',
           price: 60,
@@ -28,7 +28,7 @@ let Products = JSON.parse(localStorage.getItem('product'))
         {
           image: 'https://i.postimg.cc/m2pWqV9G/bota-mochi.jpg',
           id: 2,
-          make: 'Bota Mochi',
+          name: 'Bota Mochi',
           description:
             'This is a mochi where the sweet red bean filling surrounds the chewy mochi goodness.',
           price: 65,
@@ -36,7 +36,7 @@ let Products = JSON.parse(localStorage.getItem('product'))
         {
           image: 'https://i.postimg.cc/htYLCCqw/Yatsuhashi-mochi.jpg',
           id: 3,
-          make: 'Yatsuhashi Mochi',
+          name: 'Yatsuhashi Mochi',
           description:
             'This kind of mochi very unique cause of it shape and because it is filled with cinnamon. ',
           price: 70,
@@ -44,7 +44,7 @@ let Products = JSON.parse(localStorage.getItem('product'))
         {
           image: 'https://i.postimg.cc/g09wX1pY/mochi-icecream.jpg',
           id: 4,
-          make: 'Ice-Cream Mochi',
+          name: 'Ice-Cream Mochi',
           description:
             "As if mochi, couldn't get any better, this mochi is filled with ice cream and you can pick any flavour you fancy.",
           price: 70,
@@ -52,7 +52,7 @@ let Products = JSON.parse(localStorage.getItem('product'))
         {
           image: 'https://i.postimg.cc/W4rk5T8F/Kusa-mochi.jpg',
           id: 5,
-          make: 'Kusa Mochi',
+          name: 'Kusa Mochi',
           description:
             'This mochi is filled with the sweet red bean paste and give of a hint of green tea.',
           price: 75,
@@ -70,10 +70,11 @@ function displayProducts() {
                 <div class="card">
                     <img src="${product.image}" class="card-img-top" alt="${product.id}">
                     <div class="card-body">
-                        <h5 class="card-title">${product.make}</h5>
+                        <h5 class="card-title">${product.name}</h5>
                         <p class="card-text">${product.description}</p>
                         <p class="card-text">${product.price}</p>
-                        <a class="btn btn-primary" href="../html/checkout.html" role="button">Add to cart</a>
+                        <button class="btn btn-dark">      <span class="spinner-border spinner-border-sm d-none" role="status" aria-hidden="true"></span>Add to Cart
+</button>
                     </div>
                 </div>`;
     });
@@ -101,10 +102,11 @@ searchProduct.addEventListener('keyup', () => {
                     <div class="card">
                         <img src="${item.image}" class="card-img-top" alt="${item.id}">
                         <div class="card-body">
-                            <h5 class="card-title">${item.make}</h5>
+                            <h5 class="card-title">${item.name}</h5>
                             <p class="card-text">${item.description}</p>
                             <p class="card-text">${item.price}</p>
-                            <a class="btn btn-primary" href="../html/checkout.html" role="button">Add to cart</a>
+                            <button class="btn btn-dark" data-add-to-cart>      <span class="spinner-border spinner-border-sm d-none" role="status" aria-hidden="true"></span>Add to Cart
+</button>
                         </div>
                     </div>
                 `;
@@ -116,38 +118,196 @@ searchProduct.addEventListener('keyup', () => {
 
 // Sort product
 
-
-
 let sortBtn = document.querySelector('[data-sort]');
 
 let highest = false;
 
 sortBtn?.addEventListener('click', function () {
-    highest = highest ? false : true;
-    productWrapper.innerHTML = '';
-    let p = []
-    if (highest) {
-        p = Products.sort((a, b) => {
-            return a.price - b.price
-        })
-    } else {
-        p = Products.sort((a, b) => {
-            return b.price - a.price
-        });
-    }
-    console.log(p)
-    for (let item of Products) { 
-        productWrapper.innerHTML += `
+  highest = highest ? false : true;
+  productWrapper.innerHTML = '';
+  let p = [];
+  if (highest) {
+    p = Products.sort((a, b) => {
+      return a.price - b.price;
+    });
+  } else {
+    p = Products.sort((a, b) => {
+      return b.price - a.price;
+    });
+  }
+  console.log(p);
+  for (let item of Products) {
+    productWrapper.innerHTML += `
                     <div class="card">
                         <img src="${item.image}" class="card-img-top" alt="${item.id}">
                         <div class="card-body">
-                            <h5 class="card-title">${item.make}</h5>
+                            <h5 class="card-title">${item.name}</h5>
                             <p class="card-text">${item.description}</p>
                             <p class="card-text">${item.price}</p>
-                            <a class="btn btn-primary" href="../html/checkout.html" role="button">Add to cart</a>
+                            <button class="btn btn-dark" data-add-to-cart>      <span class="spinner-border spinner-border-sm d-none" role="status" aria-hidden="true"></span>Add to Cart
+</button>
                         </div>
                     </div>
                 `;
-    }
-})
+  }
+});
 
+
+
+// add to cart
+
+
+
+
+
+// document.addEventListener('DOMContentLoaded', function () {
+ 
+  document.addEventListener('click', function (event) {
+    if (
+      event.target &&
+      event.target.getAttribute('data-add-to-cart') !== null
+    ) {
+      const productId = parseInt(
+        event.target.getAttribute('data-add-to-cart'),
+        10
+      );
+      const product = Products.find((product) => product.id === productId);
+
+      if (product) {
+        addToCart(product);
+      }
+    }
+  });
+
+
+function displayProducts() {
+  let productWrapper = document.querySelector('[data-products]');
+  productWrapper.innerHTML = '';
+
+  Products.forEach((product) => {
+    productWrapper.innerHTML += `
+            <div class="card">
+                <img src="${product.image}" class="card-img-top" alt="${
+      product.id
+    }">
+                <div class="card-body">
+                    <h5 class="card-title">${product.make}</h5>
+                    <p class="card-text">${product.description}</p>
+                    <p class="card-text">${product.price}</p>
+                    <button class="btn btn-dark" data-add-to-cart="${
+                      product.id
+                    }" onclick="addToCart(${JSON.stringify(
+      product
+    )})"><span class="spinner-border spinner-border-sm d-none" role="status" aria-hidden="true"></span>Add to Cart</button>
+                </div>
+            </div>`;
+  });
+}
+
+function addToCart(product) {
+  const cartItems = JSON.parse(localStorage.getItem('cart')) || [];
+
+  const existingItem = cartItems.find((item) => item.id === product.id);
+
+  if (existingItem) {
+   
+    existingItem.quantity += 1;
+  } else {
+    // If the product is not in the cart, add it to the cart array
+    cartItems.push({
+      id: product.id,
+      name: product.make,
+      price: product.price,
+      quantity: 1,
+    });
+  }
+
+  localStorage.setItem('cart', JSON.stringify(cartItems));
+
+
+  alert(`${product.make} added to cart!`);
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+  updateCartInfo();
+
+  document.addEventListener('click', function (event) {
+    if (
+      event.target &&
+      event.target.getAttribute('data-add-to-cart') !== null
+    ) {
+      const productId = parseInt(
+        event.target.getAttribute('data-add-to-cart'),
+        10
+      );
+      const product = Products.find((product) => product.id === productId);
+
+      if (product) {
+        addToCart(product);
+        updateCartInfo(); 
+      }
+    }
+  });
+});
+
+function displayProducts() {
+  let productWrapper = document.querySelector('[data-products]');
+  productWrapper.innerHTML = '';
+
+  Products.forEach((product) => {
+    productWrapper.innerHTML += `
+            <div class="card">
+                <img src="${product.image}" class="card-img-top" alt="${
+      product.id
+    }">
+                <div class="card-body">
+                    <h5 class="card-title">${product.make}</h5>
+                    <p class="card-text">${product.description}</p>
+                    <p class="card-text">${product.price}</p>
+                    <button class="btn btn-dark" data-add-to-cart="${
+                      product.id
+                    }" onclick="addToCart(${JSON.stringify(
+      product
+    )})">  <span class="spinner-border spinner-border-sm d-none" role="status" aria-hidden="true"></span> Add to Cart
+</button>
+                </div>
+            </div>`;
+  });
+}
+
+function addToCart(product) {
+  const cartItems = JSON.parse(localStorage.getItem('cart')) || [];
+
+  const existingItem = cartItems.find((item) => item.id === product.id);
+
+  if (existingItem) {
+    existingItem.quantity += 1;
+  } else {
+
+    cartItems.push({
+      id: product.id,
+      name: product.make,
+      price: product.price,
+      quantity: 1,
+    });
+  }
+
+  
+  localStorage.setItem('cart', JSON.stringify(cartItems));
+
+   updateCartInfo();
+}
+
+function updateCartInfo() {
+
+  const cartItems = JSON.parse(localStorage.getItem('cart')) || [];
+
+  const totalItems = cartItems.reduce((acc, item) => acc + item.quantity, 0);
+  const totalPrice = cartItems.reduce(
+    (acc, item) => acc + item.price + item.quantity,
+    0
+  );
+
+  document.getElementById('totalItems').textContent = totalItems;
+  document.getElementById('totalPrice').textContent = totalPrice.toFixed(1);
+}
